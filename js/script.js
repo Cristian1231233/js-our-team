a46c0d83ae6efff920114c902fdd8e1da7183175
Cristian1231233 <campagnacristian22@gmail.com>
2021-11-04 14:56:49 +0100

    init commit

---
+++ js/script.js
@@ -0,0 +1,71 @@
+
+
+// creiamo il nostro array di oggetti
+// elementi dell'array:
+    //   img
+    //   titolo
+    //   info
+// creiamo un ciclo for dell'array
+// stampiamo la card a schermo
+
+const arrPeople = [
+    {
+        foto: 'wayne-barnett-founder-ceo.jpg',
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO'
+    },
+    {
+        foto: 'wayne-barnett-founder-ceo.jpg',
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO'
+    },
+    {
+        foto: 'wayne-barnett-founder-ceo.jpg',
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO'
+    },
+    {
+        foto: 'wayne-barnett-founder-ceo.jpg',
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO'
+    },
+    {
+        foto: 'wayne-barnett-founder-ceo.jpg',
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO'
+    },
+    {
+        foto: 'wayne-barnett-founder-ceo.jpg',
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO'
+    },
+    {
+        foto: 'wayne-barnett-founder-ceo.jpg',
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO'
+    },
+    {
+        foto: 'wayne-barnett-founder-ceo.jpg',
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO'
+    },
+    {
+        foto: 'wayne-barnett-founder-ceo.jpg',
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO'
+    },
+    {
+        foto: 'wayne-barnett-founder-ceo.jpg',
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO'
+    },
+    
+       
+
+]
+
+console.log(arrPeople);
+for( let i = 0; i < arrPeople.length; i++){
+    const peolpes = arrPeople[i];
+    console.log(peoples);
+}
